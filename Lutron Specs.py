1a0f0df19937a4f7b779e5f04b5b6653e976f36c
julian-kizanis-sav <55302122+julian-kizanis-sav@users.noreply.github.com>
2019-09-16 12:50:31 -0600

    Update Lutron Specs.py

--- Lutron Specs.py
+++ Lutron Specs.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
@@ -13,27 +14,7 @@ Alphabet = driver.find_element_by_xpath("(//li[@class='hasMore'])[1]")
 hover = ActionChains(driver).move_to_element(Alphabet)
 hover.perform()
 
-
-ProductCategory = driver.find_element_by_xpath("(//a[@class='step1Done'])[11]")
-if ProductCategory.is_displayed():
-    ProductCategory.click()
-else:
-    Alphabet = driver.find_element_by_xpath("(//li[@class='hasMore'])[1]")
-    hover = ActionChains(driver).move_to_element(Alphabet)
-    hover.perform()
-    ProductCategory = driver.find_element_by_xpath("(//a[@class='step1Done'])[11]")
-    ProductCategory.click()
-
-
-time.sleep(10)
-
-
-page_source = driver.page_source
-
-soup = BeautifulSoup(page_source, 'lxml')
-
-
-   
+modelCounter = 1
 ModelName = []
 ModelLanguage = []
 ModelPdf = []
@@ -41,34 +22,53 @@ pdf = ""
 languagePdf = ""
 nameCheck = 0
 row = 0
-p = 0
-nameList1 = soup.find_all("td", {"style":"width:15%;"})
-for name in nameList1:    
-    p += 1
-    if p%10 == 0:
-        print(p)
-    tempLink = name.find('a')
-    if tempLink == None:
-        if nameCheck == 0:
-            nameCheck = 1
-        else:
-            ModelLanguage.insert(row, languagePdf)
-            ModelPdf.insert(row, pdf)
-        row += 1
-        ModelName.insert(row, name.get_text())
-    else:
-        languagePdf = name.get_text()
-        pdf = tempLink['href']
-        ModelLanguage.insert(row, languagePdf)   
-        ModelPdf.insert(row, pdf)
-        nameCheck = 0
+done = 0
+print("0")
 
-for elem in range(len(ModelName)):
-    print(ModelName[elem])
-    print(ModelLanguage[elem])
-    print(ModelPdf[elem])
-    print("New Model!!")
+while done == 0:
+    ProductCategory = driver.find_element_by_xpath("(//a[@class='step1Done'])[11]")
+
+    while ProductCategory.is_displayed() == 0:
+        print("1")
+        Alphabet = driver.find_element_by_xpath("(//li[@class='hasMore'])[1]")
+        hover = ActionChains(driver).move_to_element(Alphabet)
+        hover.perform()
+    print("2")
+    ProductCategory = driver.find_element_by_xpath("(//a[@class='step1Done'])[modelCounter]")
+    ProductCategory.click()
+    modelCounter += 1
+
     
-df = pd.DataFrame(list(zip(ModelName, ModelLanguage, ModelPdf)), columns =['Name', 'Language' , 'Url'])
+    time.sleep(10)
+    
 
-export_csv = df.to_csv (r'C:\Users\Julian.Kizanis\Documents\Lutron Data Sheets\All Data Sheets.csv', header=True) #Don't forget to add '.csv' at the end of the path
+    page_source = driver.page_source
+    
+    soup = BeautifulSoup(page_source, 'lxml')
+    
+    
+       
+    
+    nameList1 = soup.find_all("td", {"style":"width:15%;"})
+    for name in nameList1:    
+    
+        tempLink = name.find('a')
+        if tempLink == None:
+            if nameCheck == 0:
+                nameCheck = 1
+            else:
+                ModelLanguage.insert(row, languagePdf)
+                ModelPdf.insert(row, pdf)
+            row += 1
+            ModelName.insert(row, name.get_text())
+        else:
+            languagePdf = name.get_text()
+            pdf = tempLink['href']
+            ModelLanguage.insert(row, languagePdf)   
+            ModelPdf.insert(row, pdf)
+            nameCheck = 0
+
+#except NoSuchElementException:    
+print(4)
+df = pd.DataFrame(list(zip(ModelName, ModelPdf)), columns =['Name', 'Url'])
+export_csv = df.to_csv (r'C:\Users\Julian.Kizanis\Documents\Lutron Data Sheets\Ballasts.csv', header=True) #Don't forget to add '.csv' at the end of the path
